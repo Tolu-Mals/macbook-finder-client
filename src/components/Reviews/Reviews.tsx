4517a7b6d96d690987bd251dc6edf3792465a091
Tolulope Malomo <malomo.tolz@gmail.com>
2024-09-02 00:20:52 +0100

    update cron, ui

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -13,7 +13,7 @@ const Reviews = () => {
       }}>Feel free to let me know what you think about Macbook finder</Text>
 
       <form action="https://formsubmit.co/malomo.tolz@gmail.com" method='POST'>
-        <Textarea maxWidth="500px" borderColor="gray.400" backgroundColor="gray.600" py="15px" _hover={{ borderColor: 'gray.400' }} _placeholder={{ color: 'gray.500' }} color="white" placeholder="Type your thoughts here" mb={4} name="Review" />
+        <Textarea maxWidth="500px" borderColor="gray.400" backgroundColor="gray.600" py="15px" _hover={{ borderColor: 'gray.400' }} _placeholder={{ color: 'gray.300' }} color="white" placeholder="Type your thoughts here" mb={4} name="Review" />
         <br />
         <Button type="submit"
           colorScheme="twitter"
